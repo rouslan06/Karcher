39ab4356ceea0dc7395d379f24f9136468261422
Laura <madiouma@air-laura.home>
2022-11-08 16:20:25 +0100

    correction erreur 2

--- index.php
+++ index.php
@@ -6,25 +6,6 @@
     require 'classes/Car.class.php';
 
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") { 
-
-    $product = new Car(
-      $model = $_POST["model"],
-      $brand = $_POST["brand"],
-      $year = $_POST["year"],
-      $price = $_POST["price"],
-      $date = $_POST["date"],
-      $image= $_POST["image"],
-      $description = $_POST["description"],
-
-    );
-
-    var_dump($product);
-
-    $dbh = new PDO("mysql:dbname=Karcher;host=127.0.0.1;port=8889", "root", "root");
- 
-
-}
 ?>
 
 <html>
@@ -40,27 +21,48 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         <?php
             menu();
+
+
         ?>
 
-<h1>Déposer une annonce</h1>
-  <section class="section-products">
-    <div class="container">
-      <div class="row justify-content-center text-center">
-        <form action="" method="POST">
-          <input type="text" name="model" placeholder="model" required />
-          <input type="text" name="brand" placeholder="brand" required />
-          <input type="text" name="year" placeholder="year" required />
-          <input type="text" name="price" placeholder="price" required />
-          <input type="date" name="date" placeholder="date" required />
-          <input type="text" name="image" placeholder="url de l'image" required />
-          <textarea name="description"></textarea>
-          <button type="submit">Déposer</button>
-        </form>
-      </div>
-    </div>
-  </section>
+        <h1>Déposer une annonce</h1>
+        <section class="section-products">
+            <div class="container">
+            <div class="row justify-content-center text-center">
+                <form action="index.php" method="POST">
+                <input type="text" name="model" placeholder="model" required />
+                <input type="text" name="brand" placeholder="brand" required />
+                <input type="text" name="year" placeholder="year" required />
+                <input type="text" name="price" placeholder="price" required />
+                <input type="date" name="date" placeholder="date" required />
+                <input type="text" name="image" placeholder="url de l'image" required />
+                <textarea name="description"></textarea>
+                <button type="submit">Déposer</button>
+                </form>
+            </div>
+            </div>
+        </section>
+
+        <?php
+
+            if ($_SERVER["REQUEST_METHOD"] == "POST") { 
+
+                $product = new Car(
+                $_POST["model"],
+                $_POST["brand"],
+                $_POST["year"],
+                $_POST["price"],
+                $_POST["date"],
+                $_POST["image"],
+                $_POST["description"],
+                );
+
+                
 
 
+            }
 
+
+        ?>
     </body>
 </html>
