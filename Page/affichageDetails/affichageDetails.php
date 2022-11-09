e657b8496353d41487b7ecfdf236470d0156238a
Rouslan abaev <abaev.rouslan@hotmail.fr>
2022-11-09 12:21:18 +0100

    ajout page detail

--- Page/affichageDetails/affichageDetails.php
+++ Page/affichageDetails/affichageDetails.php
@@ -2,6 +2,7 @@
 
 <?php
     require __DIR__."/../../Component/menu2.php";
+    //require __DIR__."/../affichageListe/affichageListe.php";
 ?>
 
 <html>
@@ -19,9 +20,42 @@
             menu2();
         ?>
 
-        <br> <br> <br> <br>
-
         <p id="center">AFFICHAGE DETAILS</p>
 
+        <section id="formulaire">
+            <?php
+                $id = htmlspecialchars($_GET["id"]);
+
+                $dbh = new PDO("mysql:dbname=karcher;host=127.0.0.1;port=8889", "root", "root");
+
+                $dbh->beginTransaction();
+
+                $auteur = $dbh->query("
+                    SELECT *
+                    FROM product 
+                    WHERE id=$id
+                ");
+
+                $result = $auteur->fetchAll(PDO::FETCH_ASSOC);
+
+                foreach ($result as $key => $voiture) {
+
+                    echo "<section id='laura2'>";
+                    echo "<div id='affichage2'>";
+
+                    echo "<p name='id' value='" . $key . " '> ";
+                    echo "<img src=" . $voiture['image'] . "> <br> <br>";
+                    echo "Modèle : " . $voiture['model'] . "<br> <br>";
+                    echo "Marque : " . $voiture['brand'] . "<br> <br>";
+                    echo "Années : " . $voiture['year'] . "<br> <br>";
+                    echo "Prix : " . $voiture['price'] . "<br> <br>";
+                    echo "Description : " . '<br> <br>' . $voiture['description'] . "<br> <br>";
+    
+                    echo "</div>";
+                    echo "</section";
+
+                }
+            ?>
+        </section>
     </body>
 </html>
--- Page/affichageListe/affichageListe.php
+++ Page/affichageListe/affichageListe.php
@@ -19,9 +19,46 @@
             menu2();
         ?>
 
-        <br> <br> <br> <br>
-
         <p id="center">AFFICHAGE LISTE</p>
 
+        <section id="details">
+
+            <?php
+                $dbh = new PDO("mysql:dbname=karcher;host=127.0.0.1;port=8889", "root", "root");
+                
+                $dbh->beginTransaction();
+
+                $auteur = $dbh->query("
+                    SELECT *
+                    FROM product
+                ");
+
+                $result = $auteur->fetchAll(PDO::FETCH_ASSOC);
+
+                foreach ($result as $key => $voiture) {
+
+                    echo "<section id='laura'>";
+                    echo "<div id='affichage'>";
+                    echo "<form id='formulaire' action='../affichageDetails/affichageDetails.php' method='post'/>";
+
+                    echo "<p name='id' value='" . $key . " '> ";
+                    echo "<img src=" . $voiture['image'] . "> <br> <br>";
+                    echo "Marque : " . $voiture['brand'] . "<br> <br>";
+                    echo "Prix : " . $voiture['price'] . "<br> <br>";
+          
+                    echo "<input id='voirPlus' type='submit' value='VOIR PLUS' />";
+
+                    echo "</form>";
+                    echo "</div>";
+                    echo "</section";
+
+                }
+            ?>
+
+        </section>
+        
+        <?php
+            //détails : image, modèle, marque, prix base, année, description, valeur enchère actuelle
+        ?>
     </body>
 </html>
